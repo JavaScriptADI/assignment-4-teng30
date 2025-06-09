c0e5cd377733e65e135a73f0ea1101ebe896a0bd
teng30 <t.khachidze30@gmail.com>
2025-06-09 19:35:23 +0400

    shop inventory

--- task1.js
+++ task1.js
@@ -1,20 +1,20 @@
 let num=Math.ceil(Math.random() * 100);
 
 if (num/3 == Math.floor(num/3) && num/5 != Math.floor(num/5)){
-console.log(`${num}\nfizz`);    
+    console.log(`${num} \nfizz`);    
 } else if (num/3 != Math.floor(num/3) && num/5 == Math.floor(num/5)){
-console.log(`${num}\nbuzz`);  
+    console.log(`${num} \nbuzz`);  
 } else if (num/3 == Math.floor(num/3) && num/5 == Math.floor(num/5)){
-console.log(`${num}\nfizzbuzz`); 
+    console.log(`${num} \nfizzbuzz`); 
 } else {
-console.log(`${num}`);    
+    console.log(`${num}`);    
 };
 
 //--------------------------------------------//
 
 for (let i = 0; i < 10; i++){
 let numb=Math.ceil(Math.random() * 1000);
-console.log(numb);
+    console.log(numb);
 };
 
 //--------------------------------------------//
@@ -26,7 +26,7 @@ let number=i * 2;
 sum += number
 };
 
-console.log(sum);   
+    console.log(sum);   
 
 //--------------------------------------------//
 
@@ -48,7 +48,58 @@ sum3 += i
 }    
 }    
 
-console.log(sum1 + sum2 - sum3)
+    console.log(sum1 + sum2 - sum3)
 //--------------------------------------------//
 
 // winashi miweria //
+
+    console.log(String.fromCharCode(78))
+
+num="sas dsd sasd sd s"
+nums=num.split(' ')
+
+    console.log(nums)
+    console.log(nums[2]) // gamoitana sasd //
+
+
+let x=[13, 14, 15, 16]
+
+    console.log(x[2])
+    console.log(x.length)
+
+
+if (x % 3 || x % 5){}       // ან ფუნქცია. ან ერთი ან მეორე. არ გულისხმობს 15-ზე გამყოფებს.//
+
+//--------------------------------------------//
+
+let someone = {
+    intelegence: "high",
+    height: "180cm",
+    weight: "80kg",
+    job: "school",
+    age: 16
+}
+
+console.log(someone)
+
+
+
+console.log(someone)
+console.log(someone.height)
+
+someone.age = 17
+console.log(someone)
+
+//----------------------------------------//
+
+let number = 12345678987654321
+let n=string(number).split("")
+
+n[Math.floor(n.length()/2)] = "5"
+
+n = n.join("")
+n = Number(n)
+
+console.log(n)
+ 
+//-----------------------------------------//
